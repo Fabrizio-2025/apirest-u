82a7b2c9a47a2e3a472837e5f4aaf72f1989c534
Fabrizio-2025 <fabriziobc2020@outlook.com>
2023-05-23 19:57:06 -0500

    first commit

---
+++ src/main/java/com/ar/ApirestuApplication.java
@@ -0,0 +1,13 @@
+package com.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApirestuApplication.class, args);
+    }
+
+}
